7fa5a427a2226a181661e5c38f7803196bff7034
Adesh Shah <shah0150@algonquinlive.com>
2023-10-30 17:17:47 -0400

    Api

--- TestAPI/ContentView.swift
+++ TestAPI/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State var posts: [Post] = []
+        //MARK: View
+        var body: some View {
+            NavigationStack {
+                List(posts) { post in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(post.title).bold().lineLimit(1).font(.title3)
+                            Text(post.body).lineLimit(1).font(.footnote)
+                        }
+                        Spacer()
+                        Text("Post #: \(post.id)")
+                    }
+                }
+                .navigationTitle("Posts from API")
+                .onAppear {
+                    fetchData()
+                }
+            }
         }
-        .padding()
+        //MARK: Model
+        struct Post: Codable, Identifiable {
+            let id: Int
+            let title, body: String
+        }
+        //MARK: Function to fetch post data (ViewModel)
+    
+    private func fetchData() {
+        //Parse URL
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let data = data {
+                do {
+                    //Parse JSON
+                    let decodedData = try JSONDecoder().decode([Post].self, from: data)
+                    self.posts = decodedData
+                } catch {
+                    //Print JSON decoding error
+                    print("Error decoding JSON: \(error.localizedDescription)")
+                }
+            } else if let error = error {
+                //Print API call error
+                print("Error fetching data: \(error.localizedDescription)")
+            }
+        }.resume()
     }
 }
 
