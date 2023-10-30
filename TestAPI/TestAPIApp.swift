0d617364073757616d922ad8bfcbc579fbe95efa
Adesh Shah <shah0150@algonquinlive.com>
2023-10-30 15:14:05 -0400

    Initial Commit

---
+++ TestAPI/TestAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAPIApp.swift
+//  TestAPI
+//
+//  Created by Adesh Shah on 2023-10-30.
+//
+
+import SwiftUI
+
+@main
+struct TestAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
